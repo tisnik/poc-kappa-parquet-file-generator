52b77673bd3088397e7b66473a45748f9c388539
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-10 13:50:50 +0100

    Very first version of DB writer

---
+++ db-writer/db-writer.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+	"log"
+)
+
+// broker address we need to communicate to
+const brokerAddress = "localhost:9092"
+
+type Metadata struct {
+	ClusterID            string `json:"cluster_id"`
+	ExternalOrganization string `json:"external_organization"`
+}
+
+type Report struct {
+	Path     string   `json:"path"`
+	Metadata Metadata `json:"metadata"`
+}
+
+func New(brokerAddress string) (sarama.Consumer, error) {
+	consumer, err := sarama.NewConsumer([]string{brokerAddress}, nil)
+	if err != nil {
+		log.Fatal("error creating consumer", err)
+		return nil, err
+	}
+	log.Print("consumer has been created")
+
+	return consumer, nil
+}
+
+func main() {
+	consumer, err := New(brokerAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := consumer.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	partitionConsumer, err := consumer.ConsumePartition("test_partitions_4", 0, sarama.OffsetOldest)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := partitionConsumer.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	consumed := 0
+	for {
+		msg := <-partitionConsumer.Messages()
+		fmt.Printf("Consumed message with key '%s' at offset %d\n", msg.Key, msg.Offset)
+		consumed++
+	}
+
+	fmt.Printf("Consumed: %d\n", consumed)
+}
--- db-writer/go.mod
+++ db-writer/go.mod
@@ -1,3 +1,5 @@
 module db-writer
 
 go 1.13
+
+require github.com/Shopify/sarama v1.27.2
