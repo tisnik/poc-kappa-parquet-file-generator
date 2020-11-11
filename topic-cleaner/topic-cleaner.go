85e009ef4bceb28418e9957f0729b0f6ba26a0c7
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-11 08:09:05 +0100

    Added missing comments

--- topic-cleaner/topic-cleaner.go
+++ topic-cleaner/topic-cleaner.go
@@ -23,55 +23,72 @@ import (
 	"github.com/Shopify/sarama" // Sarama 1.22.0
 )
 
-// broker address we need to communicate to
+// defaultBrokerAddress represents address of broker running locally.
+// Usually we need to communicate with this broker.
 const defaultBrokerAddress = "localhost:9092"
 
+// deleteTopic function deletes existing topic from Kafka
 func deleteTopic(brokerAddress string, topicName string) {
-	brokerAddrs := []string{brokerAddress}
+	brokerAddresses := []string{brokerAddress}
 
 	// configuration handling
+	// please note the versioning, it needs to be specified explicitly
 	config := sarama.NewConfig()
 	config.Version = sarama.V2_1_0_0
 
-	// try to create cluster admin object
-	clusterAdmin, err := sarama.NewClusterAdmin(brokerAddrs, config)
+	// try to create and initialize cluster admin object
+	// (it will reach out Kafka broker, so it checks the connection as well)
+	clusterAdmin, err := sarama.NewClusterAdmin(brokerAddresses, config)
+
+	// check if cluster admin has been initialized successfully
 	if err != nil {
 		log.Fatal("Error: create cluster admin: ", err.Error())
 	}
+
+	// everything's seems to be ok
 	log.Print("Cluster admin has been initialized")
 
 	// cluster admin needs to be closed properly
 	defer func() {
+		// try to close cluster admin
 		err := clusterAdmin.Close()
+
+		// check if cluster admin has been closed successfully
 		if err != nil {
 			log.Fatal("Error: close cluster admin: ", err.Error())
 		}
 	}()
 
-	// try to delete a new topic
+	// try to delete a new topic via cluster admin
 	err = clusterAdmin.DeleteTopic(topicName)
 	if err != nil {
 		log.Fatalf("Error: delete topic '%s': %v", topicName, err.Error())
 	}
 
-	// everything's seems to be ok
+	// everything's seems to be ok -> topic has been deleted
 	log.Printf("Topic '%s' has been deleted", topicName)
 }
 
 func main() {
+	const noTopic = ""
+
+	// filled via command line arguments
 	var topicName string
 	var brokerAddress string
 
-	flag.StringVar(&topicName, "topic", "", "topic name")
+	// find and parse all command line arguments
+	flag.StringVar(&topicName, "topic", noTopic, "topic name")
 	flag.StringVar(&brokerAddress, "broker", defaultBrokerAddress, "broker address")
 	flag.Parse()
 
-	if topicName == "" {
+	// check if topic name has been specified on command line
+	if topicName == noTopic {
 		log.Fatal("Topic name needs to be provided on CLI")
 	}
 
 	deleteTopic(brokerAddress, topicName)
 
+	// Examples how to delete existing topics:
 	// deleteTopic(brokerAddress, "test_partitions_1")
 	// deleteTopic(brokerAddress, "test_partitions_2")
 	// deleteTopic(brokerAddress, "test_partitions_4")
