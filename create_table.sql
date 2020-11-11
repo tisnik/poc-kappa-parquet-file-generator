374bc3b9456f635b06b6c890c7b5b9e452765f5a
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-11 12:12:23 +0100

    New column - key

--- create_table.sql
+++ create_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE reports (
     id                    SERIAL PRIMARY KEY,
+    key                   CHAR(1) NOT NULL,
     cluster_id            CHAR(36) NOT NULL,
     path                  VARCHAR(200) NOT NULL,
     external_organization VARCHAR(20) NOT NULL,
